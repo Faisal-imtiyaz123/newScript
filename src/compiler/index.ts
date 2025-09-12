cdc217bb023157e44729cbf53653dc0cb18662e7
Faisal-imtiyaz123 <ee23btech11019@iith.ac.in>
2025-09-12 19:09:28 +0530

    finalising

--- src/compiler/index.ts
+++ src/compiler/index.ts
@@ -6,9 +6,9 @@ import { Parser } from "./parser/Parser";
 export function runSource(code: string, onOutput?: (s: string) => void) {
   const lexer = new Lexer(code);
   const tokens = lexer.tokenize();
-  console.log(tokens)
   const parser = new Parser(tokens);
   const ast = parser.parseProgram();
+  console.log(ast)
   const outputs: string[] = [];
   const interpreter = new Interpreter((s) => { outputs.push(s); onOutput?.(s); });
   interpreter.run(ast);
